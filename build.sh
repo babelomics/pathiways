e5a94f0e1388b074dcf3b46ad886fc46396f3e04
frasator <frasator@gmail.com>
2015-03-17 12:51:35 +0100

    fixes

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 mkdir -p build
-rm -rf build/index.html build/index.js build/workers build/fonts build/image
+rm -rf build/index.html build/index.js build/fonts build/image
 
 vulcanize pathiways-index.html -o build/index.html --inline --strip --csp
 
 cp -r bower_components/fontawesome/fonts build/
 cp -r src/fonts/*.woff* build/fonts/
 cp -r src/images build/
-
-mkdir -p build/workers
-cp lib/jsorolla/src/lib/worker-fileupload.js build/workers/
-
-sed -i s@WORKERS_PATH=\"lib/jsorolla/src/lib/\"@WORKERS_PATH=\"workers/\"@g build/index.js
 
 sed -i s@../bower_components/fontawesome/fonts/fontawesome-webfont.@fonts/fontawesome-webfont.@g build/index.html
 sed -i s@../src/fonts/@fonts/@g build/index.html
